25520fa4064cb6e0274d6cfa6238bdbf3b49a148
Philippe Veber <philippe.veber@gmail.com>
2025-01-03 15:50:12 +0100

    full rewrite using algorithm by de Vilhena and Pottier

--- lib/radon.mli
+++ lib/radon.mli
@@ -1,58 +1,29 @@
 type vector = Lacaml.D.vec
 type matrix = Lacaml.D.mat
 
-module Vector : sig
-  type t = vector
-  val init : int -> f:(int -> float) -> t
-end
-
-type 'a ty
-
-module DSL : sig
+module type Lang = sig
+  type 'a ty
   val tfloat : float ty
   val tvector : vector ty
   val tmatrix : matrix ty
-  val tpair : 'a ty -> 'b ty -> ('a * 'b) ty
 
   type 'a exp
+
   val float : float -> float exp
   val vector : vector -> vector exp
   val matrix : matrix -> matrix exp
 
-  val pair : 'a exp -> 'b exp -> ('a * 'b) exp
-  val fst : ('a * 'b) exp -> 'a exp
-  val snd : ('a * 'b) exp -> 'b exp
+  val let_in : 'a exp -> ('a exp -> 'b exp) -> 'b exp
+  val ( let* ) : 'a exp -> ('a exp -> 'b exp) -> 'b exp
 
-  val add : 'a ty -> 'a exp -> 'a exp -> 'a exp
-  val cos : float exp -> float exp
+  val add : 'a exp -> 'a exp -> 'a exp
+  val sub : 'a exp -> 'a exp -> 'a exp
+  val mul : 'a exp -> 'a exp -> 'a exp
+
   val diag : vector exp -> matrix exp
-  val exp : float exp -> float exp
-  val inv : float exp -> float exp
-  val log : float exp -> float exp
-  val mul : float exp -> float exp -> float exp
-  val matrix_dot : matrix exp -> matrix exp -> matrix exp
-  val matrix_dotv : matrix exp -> vector exp -> vector exp
-  val matrix_exp : matrix exp -> matrix exp
-  val matrix_expm : matrix exp -> matrix exp
-  val matrix_row_sums : matrix exp -> vector exp
-  val matrix_scale : float exp -> matrix exp -> matrix exp
-  val matrix_sub : matrix exp -> matrix exp -> matrix exp
-  val sin : float exp -> float exp
-  val vector_dot : vector exp -> vector exp -> float exp
-  val vector_exp : vector exp -> vector exp
-  val vector_mul : vector exp -> vector exp -> vector exp
-  val vector_scale : float exp -> vector exp -> vector exp
-  val vector_sum : vector exp -> float exp
 
-  val eval :
-    ('a exp -> float exp) ->
-    'a ty ->
-    'a ->
-    float
-
-  val grad :
-    ('a exp -> float exp) ->
-    'a ty ->
-    'a ->
-    float * 'a
+  type 'a obs
+  val obs : ('a exp -> float exp) -> ('a * 'a ty) -> 'a obs
 end
+
+module Diff : Lang with type 'a obs = float * 'a
